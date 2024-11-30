cc93ddd039314d72ce164a14c16303801d074cdf
Quynh Dang Thi Diem <dangdiemquynh47@gmail.com>
2024-11-30 17:37:20 +0700

    update

--- app/quan-ly-nhan-vien/page.tsx
+++ app/quan-ly-nhan-vien/page.tsx
@@ -2,16 +2,20 @@ import { axiosInstance } from "@/lib/axios";
 import { columns } from "./colums";
 import { DataTable } from "./data-table";
 import DecryptComponent from "./rsa";
+import { decryptSalary, encryptSalary } from "@/lib/rsa512";
 
 const Page = async () => {
   const data: any = await axiosInstance.get("/nhanvien");
+  const en = encryptSalary("20000", "PUBPUB");
+
   return (
     <div className="">
       <DataTable columns={columns} data={data} />
-     {/* <DecryptComponent/> */}
+
+      {/* {decryptSalary(en, privateKey)} */}
+      {/* <DecryptComponent/> */}
     </div>
   );
 };
 
 export default Page;
-
